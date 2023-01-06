0ca1735a4ddb1d82aa5b3043a1759895c630089e
Travis Jack <ondegotravis@gmail.com>
2023-01-06 20:08:41 +0300

    Empty stack pop

--- monty_opcodes2.c
+++ monty_opcodes2.c
@@ -54,7 +54,7 @@ void _pop(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
 		fclose(bus.file);
