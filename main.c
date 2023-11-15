8b8b61430227a770bc80707f37877ff7fb17055f
Travis <ondegotravis@gmail.com>
2023-11-15 23:12:16 +0300

    first step in

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point for code
+ * @argc: arguement count
+ * @argv: arguements array
+ * @Return: 0 on success
+*/
+
+int main(int argc, char **argv)
+{
+
+}
